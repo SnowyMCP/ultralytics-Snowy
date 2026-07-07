8632eaef9f902e9c182c3fe3e7da6c2a39f9e748
Snowy <77794500+SnowyMCP@users.noreply.github.com>
2026-07-07 14:32:33 +0800

    Update data.py
    Signed-off-by: Snowy <77794500+SnowyMCP@users.noreply.github.com>

--- data.py
+++ data.py
@@ -1,1 +1,21 @@
+# 
+from torchvision.datasets import MNIST
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
 
+data_train=MIST('/data',  #数据地址
+                download=True,
+                transform=tansforms.Compose([
+                transform.Resize((32,32)),
+                transforme.ToTensor()]))
+data_train=MIST('/data',  #数据地址
+                train=False,
+                download=True,
+                transform=tansforms.Compose([
+                transform.Resize((32,32)),
+                transforme.ToTensor()]))
+              
+data_train_lodar=DataLoder(data_train,batch_size=256,
+                          shuffle=Ture,num_workers=8)
+data_test_lodar=DataLoder(data_test,batch_size=1024
+                          ,num_workers=8)
